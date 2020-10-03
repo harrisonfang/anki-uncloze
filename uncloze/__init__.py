6594285eafde21882f9aa5f2e06d18a036d02a67
Harrison Fang <HarrisonFang@users.noreply.github.com>
2020-10-02 23:35:43 -0700

    Add readme and license header

--- uncloze/__init__.py
+++ uncloze/__init__.py
@@ -1,15 +1,6 @@
 """
-This is an Anki add-on for version 2.1.34. It provides a custom field filter
-called 'uncloze' which will remove the cloze deletion tags from a field.
-
-For example, if I have a note with a field Foo containing:
-    Canberra was founded in {{c1::1913}}.
-
-And I want to show the text on the front of a card without the cloze tags like:
-    Canberra was founded in 1913.
-
-Then I can use this custom field filter:
-    {{uncloze::Foo}}
+Copyright 2020 Google LLC.
+SPDX-License-Identifier: Apache-2.0
 """
 
 import re
